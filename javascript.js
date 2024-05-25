409cc4db83e9c2381a0543e1d57ef574cf1cdc01
ogunnaike1 <ogunnaikeusman17@gmail.com>
2024-05-25 23:55:35 +0100

    commit

--- javascript.js
+++ javascript.js
@@ -21,10 +21,3 @@ function deleteAllTodo(){
     allTodo = [];
     showAllTodo()
 }
-
-function showAllTodo(){
-    showTodo.innerHTML = ''
-    for(let i = 0; i < allTodo.length; i++){
-        showTodo.innerHTML +=  `<div class="border border-solid border-1">${i+1}. ${allTodo[i]}</div>`;
-    }
-}
